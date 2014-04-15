65b4b41f72bbe14d8d30da0a2a406dd08a002311
Bogdan Marian <bogdan.marian77@gmail.com>
2014-04-15 21:21:17 +0300

    Inject logger using CDI. Replaced PersistenceException with the more appropriate IllegalArgumentException when encountering invalid method arguments. Corrected logging messages.

--- src/main/java/ro/satrapu/churchmanagement/persistence/PersistenceService.java
+++ src/main/java/ro/satrapu/churchmanagement/persistence/PersistenceService.java
@@ -18,15 +18,14 @@ package ro.satrapu.churchmanagement.persistence;
 import java.io.Serializable;
 import java.util.List;
 import javax.ejb.Stateless;
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.PersistenceException;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import ro.satrapu.churchmanagement.logging.LoggerInstance;
 
 /**
  *
@@ -35,121 +34,122 @@ import org.slf4j.LoggerFactory;
 @Stateless
 public class PersistenceService {
 
-    private static final Logger logger = LoggerFactory.getLogger(PersistenceService.class);
-    @PersistenceContext
+    @Inject
+    @LoggerInstance
+    Logger logger;
+
+    @PersistenceContext(unitName = PersistenceUnits.CHURCH_MANAGEMENT)
     EntityManager entityManager;
 
     public <T extends Entity> T persist(T entity) {
         if (entity == null) {
-            throw new PersistenceException("Cannot persist null entity");
+            throw new IllegalArgumentException("Cannot persist null entity");
         }
 
-        logger.debug("Persisting entity {} ...", entity);
+        logger.debug("Persisting entity of type {} ...", entity.getClass().getName());
         entityManager.persist(entity);
-        logger.debug("Persisted entity: {}", entity);
         return entity;
     }
 
     public <T extends Entity> void remove(T entity) {
         if (entity == null) {
-            throw new PersistenceException("Cannot remove null entity");
+            throw new IllegalArgumentException("Cannot remove null entity");
         }
 
-        logger.debug("Removing entity {} ...", entity);
+        logger.debug("Removing entity of type {} ...", entity.getClass().getName());
         T mergedEntity = entityManager.merge(entity);
         entityManager.remove(mergedEntity);
-        logger.debug("Removed entity: {}", entity);
     }
 
     public <T extends Entity> T merge(T entity) {
         if (entity == null) {
-            throw new PersistenceException("Cannot merge null entity");
+            throw new IllegalArgumentException("Cannot merge null entity");
         }
 
-        logger.debug("Merging entity {} ...", entity);
+        logger.debug("Merging entity of type {} ...", entity.getClass().getName());
         T mergedEntity = entityManager.merge(entity);
-        logger.debug("Merged entity: {}", mergedEntity);
         return mergedEntity;
     }
 
     public <T extends Entity> List<T> fetch(Class<T> entityClass) {
         if (entityClass == null) {
-            throw new PersistenceException("Cannot fetch entities by using null as entity class");
+            throw new IllegalArgumentException("Cannot fetch entities by using null as entity class");
         }
 
-        logger.debug("Fetching all entities of type {} ...", entityClass);
+        logger.debug("Fetching all entities of type {} ...", entityClass.getName());
+
         CriteriaBuilder builder = entityManager.getCriteriaBuilder();
         CriteriaQuery<T> criteria = builder.createQuery(entityClass);
-        Root<T> root = criteria.from(entityClass);
-        criteria.select(root);
+        criteria.select(criteria.from(entityClass));
 
         TypedQuery<T> query = entityManager.createQuery(criteria);
         List<T> resultList = query.getResultList();
-        logger.debug("Fetched {} entities of type {}", resultList.size(), entityClass);
+        logger.debug("Fetched {} entities", resultList.size());
         return resultList;
     }
 
     public <T extends Entity> T fetch(Class<T> entityClass, Serializable entityId) {
         if (entityClass == null) {
-            throw new PersistenceException("Cannot fetch entity by using null as entity class");
+            throw new IllegalArgumentException("Cannot fetch entity by using null as entity class");
         }
 
         if (entityId == null) {
-            throw new PersistenceException("Cannot fetch entity by using null as entity id");
+            throw new IllegalArgumentException("Cannot fetch entity by using null as entity id");
         }
 
-        logger.debug("Fetching entity of type {}, using id {} ...", entityClass, entityId);
+        logger.debug("Fetching entity of type {} using id {} ...", entityClass.getName(), entityId);
         T entity = entityManager.find(entityClass, entityId);
-        logger.debug("Fetched entity: {}", entity);
         return entity;
     }
 
     public <T extends Entity> T fetchReference(Class<T> entityClass, Serializable entityId) {
         if (entityClass == null) {
-            throw new PersistenceException("Cannot fetch entity reference by using null as entity class");
+            throw new IllegalArgumentException("Cannot fetch entity reference by using null as entity class");
         }
 
         if (entityId == null) {
-            throw new PersistenceException("Cannot fetch entity reference by using null as entity id");
+            throw new IllegalArgumentException("Cannot fetch entity reference by using null as entity id");
         }
 
-        logger.debug("Fetching entity reference of type {}, using id {} ...", entityClass, entityId);
+        logger.debug("Fetching entity reference of type {}, using id {} ...", entityClass.getName(), entityId);
         T entity = entityManager.getReference(entityClass, entityId);
-        logger.debug("Fetched entity reference: {}", entity);
         return entity;
     }
 
     public <T extends Entity> List<T> fetch(Class<T> entityClass, int firstResult, int maxResults) {
         if (entityClass == null) {
-            throw new PersistenceException("Cannot fetch entities using null as entity class");
+            throw new IllegalArgumentException("Cannot fetch entities using null as entity class");
         }
 
-        logger.debug("Fetching maximum {} entities of type {}, starting from index {} ...",
-                new Object[]{maxResults, entityClass, firstResult});
+        logger.debug("Fetching maximum {} entities of type {} for page {} ...",
+                maxResults, entityClass.getName(), firstResult + 1);
+
         CriteriaBuilder builder = entityManager.getCriteriaBuilder();
         CriteriaQuery<T> criteria = builder.createQuery(entityClass);
-        Root<T> root = criteria.from(entityClass);
-        criteria.select(root);
+        criteria.select(criteria.from(entityClass));
 
         TypedQuery<T> query = entityManager.createQuery(criteria);
         query.setFirstResult(firstResult);
         query.setMaxResults(maxResults);
+
         List<T> resultList = query.getResultList();
-        logger.debug("Fetched {} entities of type {}", resultList.size(), entityClass);
+        logger.debug("Found {} entities", resultList.size());
         return resultList;
     }
 
     public <T extends Entity> long count(Class<T> entityClass) {
         if (entityClass == null) {
-            throw new PersistenceException("Cannot count entities by using null as entity class");
+            throw new IllegalArgumentException("Cannot count entities by using null as entity class");
         }
 
-        logger.debug("Counting entities {} ...", entityClass);
+        logger.debug("Counting entities of type {} ...", entityClass.getName());
+
         CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
         CriteriaQuery<Long> criteriaQuery = criteriaBuilder.createQuery(Long.class);
         criteriaQuery.select(criteriaBuilder.count(criteriaQuery.from(entityClass)));
+
         long count = entityManager.createQuery(criteriaQuery).getSingleResult();
-        logger.debug("Counted {} entities of type {}", count, entityClass);
+        logger.debug("Found {} entities", count);
         return count;
     }
 }
