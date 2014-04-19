8c008e1a87b305a1b5af22824d8e20099ca78e3f
Bogdan Marian <bogdan.marian77@gmail.com>
2014-04-19 22:27:53 +0300

    Removed incorrect constraint associated with optional value.

--- src/main/java/ro/satrapu/churchmanagement/persistence/Person.java
+++ src/main/java/ro/satrapu/churchmanagement/persistence/Person.java
@@ -19,7 +19,6 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Null;
 import javax.validation.constraints.Size;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -37,17 +36,16 @@ import lombok.ToString;
 public class Person extends ManagedEntity {
 
     @NotNull
-    @Size(min = 2, max = 400)
+    @Size(min = 1, max = 400)
     @Column(nullable = false, length = 400, name = "FirstName")
     private String firstName;
 
-    @Null
-    @Size(min = 2, max = 400)
+    @Size(max = 400)
     @Column(nullable = true, length = 400, name = "MiddleName")
     private String middleName;
 
     @NotNull
-    @Size(min = 2, max = 400)
+    @Size(min = 1, max = 400)
     @Column(nullable = false, length = 400, name = "LastName")
     private String lastName;
 }
