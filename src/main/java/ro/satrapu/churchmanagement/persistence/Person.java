cacbff860681be0f946c1be464a4ff0c639398fb
Bogdan Marian <bogdan.marian77@gmail.com>
2014-06-23 23:09:52 +0300

    Added unique constraint for a person's email address.

--- src/main/java/ro/satrapu/churchmanagement/persistence/Person.java
+++ src/main/java/ro/satrapu/churchmanagement/persistence/Person.java
@@ -19,6 +19,7 @@ import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import lombok.Data;
@@ -30,7 +31,9 @@ import lombok.ToString;
  * @author satrapu
  */
 @Entity
-@Table(name = "Persons")
+@Table(name = "Persons", uniqueConstraints = {
+    @UniqueConstraint(columnNames = {"EmailAddress"}, name = "UK_Persons_EmailAddress")
+})
 @Data
 @EqualsAndHashCode(callSuper = true)
 @ToString(callSuper = true)
