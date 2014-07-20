f57361d5f5c9e8b866ce4772e236a4704905cfef
Bogdan Marian <bogdan.marian77@gmail.com>
2014-07-20 15:55:58 +0300

    Changed Authenticator class into an interface with an implementation.

---
+++ src/main/java/ro/satrapu/churchmanagement/security/HardCodedUserAuthenticator.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2014 satrapu.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ro.satrapu.churchmanagement.security;
+
+import javax.enterprise.inject.Alternative;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import org.slf4j.Logger;
+import ro.satrapu.churchmanagement.logging.LoggerInstance;
+import ro.satrapu.churchmanagement.ui.FacesContextInstance;
+
+/**
+ * An {@link UserAuthenticator} implementation which authenticates users against a hard-coded pair of user named and password.
+ *
+ * @author satrapu
+ */
+@Alternative
+public class HardCodedUserAuthenticator implements UserAuthenticator {
+
+    private static final String HARD_CODED_USER_NAME = "satrapu";
+
+    /**
+     * This hard-coded password was generated using <a href="http://passwordsgenerator.net/">Secure Password Generator tool</a>.
+     */
+    private static final String HARD_CODED_PASSWORD = "QZ{vVyj*:9C5P5fBm+tQGch}mM\\,s;q>ZE7F!k6;Z@#RJpR{hbAN&GJz~4vvPF#Je{q<Q&afT]5}-@Sa[qMa;)_Bj!sb6susv>W^^{?R#t*XD4hym%CQ9_VhNTvA;*J^";
+
+    @Inject
+    @LoggerInstance
+    Logger logger;
+
+    @Inject
+    @FacesContextInstance
+    FacesContext facesContext;
+
+    @Override
+    public AuthenticatedUser authenticate(AuthenticationDetails authenticationDetails) {
+	if (authenticationDetails == null) {
+	    throw new IllegalArgumentException("Authentication details are required");
+	}
+
+	String userName = authenticationDetails.getUserName();
+	String password = authenticationDetails.getPassword();
+
+	if (userName == null || userName.length() == 0) {
+	    throw new IllegalArgumentException("User name is required");
+	}
+
+	if (password == null || password.length() == 0) {
+	    throw new IllegalArgumentException("Password is required");
+	}
+
+	boolean isAuthenticated = getHardCodedValue().getUserName().equals(userName) && getHardCodedValue().getPassword().equals(password);
+
+	if (!isAuthenticated) {
+	    throw new RuntimeException("User name and password do not match");
+	}
+
+	AuthenticatedUser authenticatedUser = new AuthenticatedUser();
+	authenticatedUser.setName("Bogdan Marian");
+	return authenticatedUser;
+    }
+
+    @Override
+    public AuthenticationDetails getHardCodedValue() {
+	AuthenticationDetails authenticationDetails = new AuthenticationDetails();
+	authenticationDetails.setUserName(HARD_CODED_USER_NAME);
+	authenticationDetails.setPassword(HARD_CODED_PASSWORD);
+
+	return authenticationDetails;
+    }
+
+    @Override
+    public boolean hasHardCodedValue() {
+	switch (facesContext.getApplication().getProjectStage()) {
+	    case Development:
+	    case SystemTest:
+	    case UnitTest:
+		return true;
+	    default:
+		return false;
+	}
+    }
+}
---
+++ src/main/java/ro/satrapu/churchmanagement/security/UserAuthenticator.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2014 satrapu.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ro.satrapu.churchmanagement.security;
+
+/**
+ * Authenticates users.
+ *
+ * @author satrapu
+ */
+public interface UserAuthenticator {
+
+    /**
+     * Checks whether the given details can be used to fetch an existing user.
+     *
+     * @param authenticationDetails Represents the details of an authentication operation.
+     * @return
+     */
+    public AuthenticatedUser authenticate(AuthenticationDetails authenticationDetails);
+
+    /**
+     * Checks whether this implementation can produce an {@link AuthenticationDetails} instance used for authenticating a hard-coded user.
+     *
+     * @return
+     */
+    public boolean hasHardCodedValue();
+
+    /**
+     * Creates an {@link AuthenticationDetails} instance used for authenticating a hard-coded user.
+     *
+     * @return An {@link AuthenticationDetails} instance if this {@link UserAuthenticator} implementation supports hard-coded users; null,
+     * otherwise.
+     */
+    public AuthenticationDetails getHardCodedValue();
+}
