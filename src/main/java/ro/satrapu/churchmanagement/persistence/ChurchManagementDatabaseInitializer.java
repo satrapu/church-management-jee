af04f919c5fd1a719db97fa4b24dbba1b3e32836
Bogdan Marian <bogdan.marian77@gmail.com>
2015-10-10 15:58:18 +0300

    Added support for initializing the underlying database with randomly generated entities.

---
+++ src/main/java/ro/satrapu/churchmanagement/persistence/ChurchManagementDatabaseInitializer.java
@@ -0,0 +1,38 @@
+package ro.satrapu.churchmanagement.persistence;
+
+import io.codearte.jfairy.Fairy;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import javax.inject.Inject;
+
+@Startup
+@Singleton
+public class ChurchManagementDatabaseInitializer {
+    private final int MAX_AMOUNT_OF_PERSONS_TO_GENERATE = 117;
+
+    @Inject
+    private PersistenceService persistenceService;
+
+    @PostConstruct
+    public void initializeDatabase() {
+        Fairy fairy = Fairy.create();
+
+        generatePersons(fairy, MAX_AMOUNT_OF_PERSONS_TO_GENERATE);
+    }
+
+    private void generatePersons(Fairy fairy, int maxAmount) {
+        for (int i = 0; i < maxAmount; i++) {
+            io.codearte.jfairy.producer.person.Person generatedPerson = fairy.person();
+
+            Person person = new Person();
+            person.setFirstName(generatedPerson.firstName());
+            person.setMiddleName(generatedPerson.middleName());
+            person.setLastName(generatedPerson.lastName());
+            person.setEmailAddress(generatedPerson.companyEmail());
+
+            persistenceService.persist(person);
+        }
+    }
+}
