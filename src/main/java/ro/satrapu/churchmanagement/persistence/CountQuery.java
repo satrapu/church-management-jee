7e13bfd7d3eeb6072220899642f8f901ae1e4a76
satrapu <bogdan.marian77@gmail.com>
2015-11-29 17:43:41 +0200

    Improved interface comments.

--- src/main/java/ro/satrapu/churchmanagement/persistence/CountQuery.java
+++ src/main/java/ro/satrapu/churchmanagement/persistence/CountQuery.java
@@ -3,11 +3,11 @@ package ro.satrapu.churchmanagement.persistence;
 import javax.persistence.EntityManager;
 
 /**
- * Represents an object used for querying a database.
+ * Represents an object used for counting records found in a database.
  */
 public interface CountQuery {
     /**
-     * Counts the entities matching the underlying search criteria.
+     * Counts the records matching the search criteria to be created using the given {@code entityManager} object.
      *
      * @param entityManager
      * @return
