b02c006982462c83596be54327b9096a2518eb17
satrapu <bogdan.marian77@gmail.com>
2015-11-29 17:47:57 +0200

    Added comments to the interface method.

--- src/main/java/ro/satrapu/churchmanagement/persistence/CountQuery.java
+++ src/main/java/ro/satrapu/churchmanagement/persistence/CountQuery.java
@@ -9,8 +9,8 @@ public interface CountQuery {
     /**
      * Counts the records matching the search criteria to be created using the given {@code entityManager} object.
      *
-     * @param entityManager
-     * @return
+     * @param entityManager The {@code EntityManager} instance to use for creating a search criteria.
+     * @return The number of records matching the search criteria.
      */
     long getTotalRecords(EntityManager entityManager);
 }
